515d3c06b08a9e6ec8bab446d0c8286675f3d824
juskevic <73663894+juskevic@users.noreply.github.com.>
2024-11-03 14:33:42 +0100

    Update layout and landing page with new UI elements
    Simplified the body class attribute in layout.tsx. Enhanced the landing page by adding two new buttons for updates information and privacy protection, along with slight adjustments to text segment spacing.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,26 @@
+import {Button} from "@/components/ui/button";
+import {ShieldQuestion, Stars} from "lucide-react";
 
 export default function LandingPage() {
     return (
         <main>
-            <div className="flex flex-col !text-center gap-4">
-                <h1 className="text-5xl font-bold bg-gradient-to-r from-gray-50 to-gray-200 bg-clip-text text-transparent">
-                    {"Počítejte svou pokladnu efektivně a přesně."}
-                </h1>
-                <p className="text-xl text-gray-200">
-                    {"S Výčetkou je počítání hotovosti hračka."}
-                </p>
+            <div className="flex flex-col !text-center gap-8">
+                <div className="space-y-4">
+                    <h1 className="text-5xl font-bold bg-gradient-to-r from-gray-50 to-gray-200 bg-clip-text text-transparent">
+                        {"Počítejte svou pokladnu efektivně a přesně."}
+                    </h1>
+                    <p className="text-xl text-gray-200">
+                        {"S Výčetkou je počítání hotovosti hračka."}
+                    </p>
+                </div>
+                <div className="flex flex-row justify-center gap-2">
+                    <Button variant="outline" className="rounded-full">
+                        Co je nového? <Stars size={20} />
+                    </Button>
+                    <Button variant="ghost" className="rounded-full">
+                        Ochrana soukromí <ShieldQuestion size={20} />
+                    </Button>
+                </div>
             </div>
         </main>
     )
